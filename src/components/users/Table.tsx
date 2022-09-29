46c3081ed13fd4eeb2bde65f69d3f91464ff2740
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-09-29 16:08:04 -0300

    added new style to table

--- src/components/users/Table.tsx
+++ src/components/users/Table.tsx
@@ -1,23 +1,27 @@
 import { Pencil, XCircle } from "phosphor-react";
+import Avatar from "./Avatar";
 
 interface TableProps {
   id: number;
+  avatar: string;
   name: string;
   office: string;
   access: string;
 }
 
-const Table = ({ id, name, office, access }: TableProps) => {
+const Table = ({ avatar, name, office, access }: TableProps) => {
   return (
-    <tr>
-      <td>{id + 1}</td>
-      <td>{name}</td>
-      <td>{office}</td>
-      <td>{access}</td>
-      <td className="flex">
+    <tr className="flex items-center bg-zinc-700 h-16 p-2 m-2 rounded-lg hover:bg-violet-800 hover:transition-colors">
+      <Avatar image={avatar} />
+      <th className="text-sm font-black mx-4">{name}</th>
+      <th className="flex flex-col mx-4">
+        <span className="text-sm font-black">{office}</span>
+        <span className="text-sm">{access}</span>
+      </th>
+      <th className="mx-4">
         <Pencil className="hover:text-zinc-300" size={25} />
         <XCircle className="hover:text-zinc-300" size={25} />
-      </td>
+      </th>
     </tr>
   );
 };
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -2,27 +2,53 @@ import Table from "../components/users/Table";
 
 const Users = () => {
   const users = [
-    { id: 0, name: "Kelvis Delapena", office: "Software developer", access: "Employee" },
-    { id: 1, name: "Fernando Silva", office: "Software developer", access: "Employee" },
-    { id: 2, name: "Stefany Delapena", office: "Inside sales", access: "Employee" },
-    { id: 3, name: "Kelly Fernanda", office: "Human recurses", access: "Admin" },
+    {
+      id: 0,
+      avatar:
+        "https://static-cdn.jtvnw.net/jtv_user_pictures/0650516e-ab8f-4407-996b-fbff5686d5a2-profile_image-70x70.png",
+      name: "Kelvis Delapena",
+      office: "Software developer",
+      access: "Employee",
+    },
+    {
+      id: 1,
+      avatar:
+        "https://static-cdn.jtvnw.net/jtv_user_pictures/0650516e-ab8f-4407-996b-fbff5686d5a2-profile_image-70x70.png",
+      name: "Fernando Silva",
+      office: "Software developer",
+      access: "Employee",
+    },
+    {
+      id: 2,
+      avatar:
+        "https://static-cdn.jtvnw.net/jtv_user_pictures/0650516e-ab8f-4407-996b-fbff5686d5a2-profile_image-70x70.png",
+      name: "Stefany Delapena",
+      office: "Inside sales",
+      access: "Employee",
+    },
+    {
+      id: 3,
+      avatar:
+        "https://static-cdn.jtvnw.net/jtv_user_pictures/0650516e-ab8f-4407-996b-fbff5686d5a2-profile_image-70x70.png",
+      name: "Kelly Fernanda",
+      office: "Human recurses",
+      access: "Admin",
+    },
   ];
   return (
-    <div className="m-10 w-full">
-      <h1>Users</h1>
-      <table className="mt-8">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Office</th>
-            <th>Access</th>
-            <th></th>
-          </tr>
-        </thead>
+    <div className="m-10">
+      <div className="mb-10">
+        <h1 className="font-black text-lg">Users</h1>
+        <h3 className="text-sm">Manage and add new users</h3>
+      </div>
+      <table>
         <tbody>
           {users.map((user, index) => {
-            return <Table key={index} {...user} />;
+            return (
+              <>
+                <Table key={index} {...user} />
+              </>
+            );
           })}
         </tbody>
       </table>
